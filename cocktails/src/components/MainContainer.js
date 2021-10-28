8188909fda0cf3baf4d5bb249fcb273b4131bb75
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-28 12:11:44 +1100

    Styling completed

--- cocktails/src/components/MainContainer.js
+++ cocktails/src/components/MainContainer.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { useCreateUserContext } from './Cocktailcontex'
 const MainContainer = () => {
-    const{data} = useCreateUserContext;
-    console.log(data)
+    const{data} = useCreateUserContext();
+const{drinks} = data;
+console.log(drinks)
     return (
         <section className='drinks-panel'>
-<h1> Displays here</h1>
+
+{
+    drinks.map((value)=>{
+        const {idDrink, strCategory, strDrinkThumb, strIngredient1, strIngredient3, strIngredient2, strGlass } = value;
+        return (
+            <div className='drinks-component' key={idDrink}>
+              
+                <img src={strDrinkThumb}/>
+                <h3> Category: {strCategory}</h3>
+                <h4>Alcoholic</h4>
+                <h5> {strGlass}</h5>
+                <button> Details</button>
+                
+
+            </div>
+        )
+
+    })
+}
         </section>
     )
 }
